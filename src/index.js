7cfb9064033b7fa471267c724181642b531630f0
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-22 10:49:42 -0600

    Add GET request to Word Watch API to return the most used word

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  $.ajax({
+    url: 'https://wordwatch-api.herokuapp.com/api/v1/top_word',
+    success: function(data) {
+      $('.top-word').append(`The most used word is '${Object.keys(data.word)[0]}'. It has a count of ${Object.values(data.word)[0]}.`)
+    },
+    error: function(error) {
+      console.log(error)
+    }
+  })
 })
